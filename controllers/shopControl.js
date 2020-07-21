05c8f1552599fc76c40794b0d3861924ebd8f5ac
Ravi Shankar <67696330+ravis-web@users.noreply.github.com>
2020-07-21 11:16:47 +0530

    pagination

--- controllers/shopControl.js
+++ controllers/shopControl.js
@@ -1,5 +1,7 @@
 const Product = require('../models/Prod');
 
+const PROD_PER_PAGE = 1;
+
 exports.indexPage = (req, res) => {
   res.render('e-shop/index', {
     docTitle: 'Home',
@@ -23,17 +25,32 @@ exports.fetchProd = (req, res, nxt) => {
 };
 
 exports.fetchProds = (req, res, nxt) => {
+  const page = {
+    curr: parseInt(req.query.page) || 1
+  };
+
   // Product.fetch() // mongodb
-  Product.find() // mongoose
-    // .select('title price -_id') // mongoose
-    // .populate('userId', 'name') // mongoose
-    .then(prods => {
-      res.render('e-shop/eshop', {
-        products: prods,
-        docTitle: 'E-Shop',
-        path: req.url,
-        user: req.user
-      });
-    })
-    .catch(err => nxt(err));
+  // Product.find() // mongoose
+  // .select('title price -_id') // mongoose
+  // .populate('userId', 'name') // mongoose
+
+  Product.find().countDocuments().then(count => {
+    page.last = Math.ceil(count / PROD_PER_PAGE);
+
+    if (page.curr > page.last || page.curr == 0) {
+      return res.redirect('/shop');
+    }
+
+    return Product.find().skip((page.curr - 1) * PROD_PER_PAGE) // mongo
+      .limit(PROD_PER_PAGE) // mongo
+      .then(prods => {
+        res.render('e-shop/eshop', {
+          products: prods,
+          docTitle: 'E-Shop',
+          path: req.url,
+          user: req.user,
+          page: page
+        });
+      })
+  }).catch(err => nxt(err));
 };
